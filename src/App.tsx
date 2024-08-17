573e880c1be00e9ce037c991ca9151703f54b116
Adrien Loup <adloup@gmail.com>
2024-08-17 16:48:55 +0200

    feature(1): init project

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,24 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { TransitionPageProvider } from './contexts/Transition';
 import HomePage from './pages/Home/Home';
 import ProductPage from './pages/Product/Product';
+import CollectionPage from './pages/Collection/Collection';
 import ShopPage from './pages/Shop/Shop';
 import ShoppingCartPage from './pages/ShoppingCart/ShoppingCart';
 
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/one" element={<HomePage />} />
-        <Route path="/one/*" element={<HomePage />} />
-        <Route path="/one/product/:id" element={<ProductPage />} />
-        <Route path="/one/shop" element={<ShopPage />} />
-        <Route path="/one/shoppingcart" element={<ShoppingCartPage />} />
-      </Routes>
+      <TransitionPageProvider>
+        <Routes>
+          <Route path="/one" element={<HomePage />} />
+          <Route path="/one/*" element={<HomePage />} />
+          <Route path="/one/collection/:id" element={<CollectionPage />} />
+          <Route path="/one/product/:id" element={<ProductPage />} />
+          <Route path="/one/shop" element={<ShopPage />} />
+          <Route path="/one/shoppingcart" element={<ShoppingCartPage />} />
+        </Routes>
+      </TransitionPageProvider>
     </BrowserRouter>
   );
 }
