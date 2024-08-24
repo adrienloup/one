870ff51999f5ccf673998722dc33456088314817
Adrien Loup <adloup@gmail.com>
2024-08-24 05:44:31 +0200

    feature(2): add data

--- src/pages/Collection/Collection.tsx
+++ src/pages/Collection/Collection.tsx
@@ -4,25 +4,27 @@ import { useCollection } from '../../hooks/useCollection';
 import { HeaderComponent } from '../../components/Header/Header';
 import { MainComponent } from '../../components/Main/Main';
 import { FooterComponent } from '../../components/Footer/Footer';
-// import { ScrollToTopComponent } from '../../components/ScrollToTop/ScrollToTop';
+import { CursorComponent } from '../../components/Cursor/Cursor';
 import { ArticleComponent } from '../../components/Article/Article';
 import { TitleComponent } from '../../components/Title/Title';
 import { LoaderComponent } from '../../components/Loader/Loader';
 import { FailureComponent } from '../../components/Failure/Failure';
 import { ImageComponent } from '../../components/Image/Image';
+import { CollectionsComponent } from '../../components/Collections/Collections';
 import styles from './Collection.module.scss';
-import { FavoritesComponent } from '../../components/Favorites/Favorites';
 
 function CollectionPage() {
-  console.log('CollectionPage');
+  // console.log('CollectionPage');
 
   const { loading, errors, collections } = useCollection();
 
-  const collection = collections?.filter(
+  const currentCollection = collections?.find(
     (collection) => collection.route === location.pathname.split('/').pop()
   );
 
-  useTitle(collection ? collection[0].title + ' Collection' : 'Collection');
+  useTitle(
+    currentCollection ? currentCollection.title + ' Collection' : 'Collection'
+  );
 
   return (
     <>
@@ -30,24 +32,27 @@ function CollectionPage() {
       <MainComponent>
         <ArticleComponent>
           {loading && <LoaderComponent cssClass={styles.loader} size="large" />}
-          {collection && (
+          {currentCollection && (
             <>
-              <TitleComponent>{collection[0].title} Collection</TitleComponent>
+              <TitleComponent>
+                {currentCollection.title} Collection
+              </TitleComponent>
               <div className={styles.content}>
                 <div className={styles.inner}>
                   <ImageComponent
                     cssClass={styles.image}
-                    src={`/one/data/collections/${collection[0].image}`}
-                    alt={collection[0].title}
+                    src={`/one/data/collections/${currentCollection.image}`}
+                    alt={currentCollection.title}
                   />
-                  <div className={styles.short}>{collection[0].short}</div>
+                  <div className={styles.short}>{currentCollection.short}</div>
                   <div className={styles.description}>
-                    {collection[0].description.map((text, index) => (
+                    {currentCollection.description.map((text, index) => (
                       <p key={index}>{text}</p>
                     ))}
                   </div>
                 </div>
               </div>
+              <CollectionsComponentMemo />
             </>
           )}
           {errors && (
@@ -55,16 +60,16 @@ function CollectionPage() {
               Failed to fetch :(
             </FailureComponent>
           )}
-          {collection && <FavoritesComponent title={collection[0].title} />}
         </ArticleComponent>
       </MainComponent>
       <FooterComponentMemo />
-      {/* <ScrollToTopComponent /> */}
+      <CursorComponent />
     </>
   );
 }
 
 const HeaderComponentMemo = memo(HeaderComponent);
+const CollectionsComponentMemo = memo(CollectionsComponent);
 const FooterComponentMemo = memo(FooterComponent);
 
 export default CollectionPage;
