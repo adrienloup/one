1665e841c612e5e49d1b70cded858e056e1a4475
Adrien Loup <adloup@gmail.com>
2024-08-24 05:45:12 +0200

    feature(1): init project

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,14 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { AllProviders } from './utils/combineProviders';
+import { version } from '../package.json';
 import App from './App.tsx';
 import './index.scss';
+
+console.log(
+  `%cOne ${version}`,
+  'padding: 2px 4px; background: #ff0057; color: #fff;'
+);
 
 createRoot(document.getElementById('_one_1em0m_3')!).render(
   <StrictMode>
